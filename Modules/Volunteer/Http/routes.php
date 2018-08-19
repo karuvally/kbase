466f04b20bd78412bae01f80c7a167abf26d04e9
Mohamed Rashid <rashivkp@gmail.com>
2018-08-20 01:30:17 +0530

    create migration for volunteer, availability

--- Modules/Volunteer/Http/routes.php
+++ Modules/Volunteer/Http/routes.php
@@ -2,5 +2,5 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'volunteer', 'namespace' => 'Modules\Volunteer\Http\Controllers'], function()
 {
-    Route::get('/', 'VolunteerController@index');
+    Route::resource('/', 'VolunteerController');
 });
