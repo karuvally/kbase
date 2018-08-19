fe3f38f4ab4390bfb2f745be104999d469049ac4
Manu Krishnan T V <tvm@dayscholars.com>
2018-08-20 00:07:28 +0530

    Inventory Stock migrations

--- database/migrations/2018_08_19_181304_create_categories_table.php
+++ database/migrations/2018_08_19_181304_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('parent_id')->unsigned();
+            $table->foreign('parent_id')->references('id')->on('categories');
+            $table->string('name', 255);
         });
     }
 
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
