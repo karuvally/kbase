bc4820ab1b348976d4bfc1b4114f5c7d68da2627
Manu Krishnan T V <tvm@dayscholars.com>
2018-08-20 23:34:33 +0530

    Add Model functions, disables timestamp in specific Models

--- app/Item.php
+++ app/Item.php
@@ -6,17 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+
+    public $timestamps = false;
+
     /**
+     * Fetch latest stock for item in a camp
      *
+     * @param int $camp_id
+     * @return object
+     */
+    public function getStock($camp_id)
+    {
+        $last_stock = Stock::where('camp_id', $camp_id)
+            ->where('item_id', $this->id)
+            ->select('availability', 'requirement', 'updated_at')
+            ->orderBy('updated_at', 'DESC')
+            ->first();
+
+        if ($last_stock) {
+            return $last_stock;
+        }
+        return false;
+    }
+
+
+    /**
      * Update stock for item in a camp, reducing entries
      *
      * @param int $camp_id
      * @param decimal $available
      * @param decimal $required
      * @return boolean
-     *
      */
-    protected function updateStock($camp_id, $availability = null, $requirement = null)
+    public function updateStock($camp_id, $availability = null, $requirement = null)
     {
         $stock = new Stock;
         $stock->camp_id = $camp_id;
@@ -26,11 +48,20 @@ class Item extends Model
 
         $last_stock = Stock::where('camp_id', $camp_id)
             ->where('item_id', $this->id)
-            ->last();
-        if (!$availability) {
-            $stock->availability = $last_stock->availability;
-        } else if (!$requirement) {
-            $stock->requirement = $last_stock->requirement;
+            ->orderBy('updated_at', 'DESC')
+            ->first();
+        if ($last_stock) {
+            if (!$availability) {
+                $stock->availability = $last_stock->availability;
+            } else if (!$requirement) {
+                $stock->requirement = $last_stock->requirement;
+            }
+        } else {
+            if (!$availability) {
+                $stock->availability = 0;
+            } else if (!$requirement) {
+                $stock->requirement = 0;
+            }
         }
 
         if ($stock->save()) {
