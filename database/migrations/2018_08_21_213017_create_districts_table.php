cbc6b5cfa526810b72d6a6f036953f5217ae03a2
Mohamed Rashid <rashivkp@gmail.com>
2018-08-21 13:50:50 +0530

    add migrations for tables - states, districts, areas, camps

---
+++ database/migrations/2018_08_21_213017_create_districts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('state_id');
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
